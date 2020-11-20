24eed9d170a6162b2104188d57f217fbcfee8ed4
Lauren Horne <lahwran0@gmail.com>
2020-11-20 15:16:10 -0700

    Revert "remove unwanted repetition and unnecessary commands"
    This reverts commit af825c8e

--- castervoice/rules/core/punctuation_rules/punctuation.py
+++ castervoice/rules/core/punctuation_rules/punctuation.py
@@ -14,23 +14,27 @@ from castervoice.lib.merge.mergerule import MergeRule
 from castervoice.lib.merge.state.short import R
 
 
-class Punctuation(MergeRule): #
+class Punctuation(MergeRule):
     pronunciation = "punctuation"
 
     mapping = {
-        "[<long>] <text_punc>":
-            R(Text("%(long)s" + "%(text_punc)s" + "%(long)s")),
+        "[<long>] <text_punc> [<npunc>]":
+            R(Text("%(long)s" + "%(text_punc)s" + "%(long)s"))*Repeat(extra="npunc"),
         # For some reason, this one doesn't work through the other function
-        "[<long>] backslash":
-            R(Text("%(long)s" + "\\" + "%(long)s")),
+        "[<long>] backslash [<npunc>]":
+            R(Text("%(long)s" + "\\" + "%(long)s"))*Repeat(extra="npunc"),
         "<double_text_punc> [<npunc>]":
             R(Text("%(double_text_punc)s") + Key("left"))*Repeat(extra="npunc"),
         "tabby [<npunc>]":
             R(Key("tab"))*Repeat(extra="npunc"),
         "(back | shin) tabby [<npunc>]":
             R(Key("s-tab"))*Repeat(extra="npunc"),
-        "space":
-            R(Text(" ")),
+        "boom [<npunc>]":
+            R(Text(", "))*Repeat(extra="npunc"),
+        "bam [<npunc>]":
+            R(Text(". "))*Repeat(extra="npunc"),
+        "ace [<npunc100>]":
+            R(Text(" "))*Repeat(extra="npunc100"),
     }
 
     extras = [
