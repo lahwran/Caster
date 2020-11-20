f75cda7498c1997aae461315b1266b4217bf1ac6
Lauren Horne <lahwran0@gmail.com>
2020-11-20 16:02:47 -0700

    delete a bunch more crap i don't want

--- _caster.py
+++ _caster.py
@@ -15,8 +15,6 @@ DependencyMan().initialize()
 from castervoice.lib import settings # requires DependencyMan to be initialized
 settings.initialize()
 
-from castervoice.lib.ctrl.updatecheck import UpdateChecker # requires settings/dependencies
-UpdateChecker().initialize()
 
 from castervoice.lib.ctrl.configure_engine import EngineConfigEarly, EngineConfigLate
 EngineConfigEarly() # requires settings/dependencies
@@ -32,9 +30,5 @@ if control.nexus() is None: # Initialize Caster State
     _content_loader = ContentLoader(_crg)
     control.init_nexus(_content_loader)
     EngineConfigLate() # Requires grammars to be loaded and nexus
-   
-if settings.SETTINGS["sikuli"]["enabled"]:
-    from castervoice.asynch.sikuli import sikuli_controller
-    sikuli_controller.get_instance().bootstrap_start_server_proxy()
 
 print("\n*- Starting " + settings.SOFTWARE_NAME + " -*")
--- castervoice/rules/core/utility_rules/caster_rule.py
+++ castervoice/rules/core/utility_rules/caster_rule.py
@@ -1,25 +1,9 @@
 from dragonfly import MappingRule, Function, RunCommand, Playback
 
 from castervoice.lib import control, utilities
-from castervoice.lib.ctrl.dependencies import find_pip  # pylint: disable=no-name-in-module
-from castervoice.lib.ctrl.updatecheck import update
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.state.short import R
 
-_PIP = find_pip()
-
-
-class _DependencyUpdate(RunCommand):
-    synchronous = True
-
-    # pylint: disable=method-hidden
-    def process_command(self, proc):
-        # Process the output from the command.
-        RunCommand.process_command(self, proc)
-        # Only reboot dragon if the command was successful and online_mode is true
-        # 'pip install ...' may exit successfully even if there were connection errors.
-        if proc.wait() == 0 and update:
-            Function(utilities.reboot).execute()
 
 
 class CasterRule(MappingRule):
@@ -28,11 +12,6 @@ class CasterRule(MappingRule):
             R(Function(utilities.clear_log)),
         "reboot caster":
             R(Function(utilities.reboot)),
-        "update dragonfly":
-            R(_DependencyUpdate([_PIP, "install", "--upgrade", "dragonfly2"])),
-        # update management ToDo: Fully implement castervoice PIP install
-        #"update caster":   
-        #    R(_DependencyUpdate([_PIP, "install", "--upgrade", "castervoice"])),
 
         # ccr de/activation
         "enable (c c r|ccr)":
